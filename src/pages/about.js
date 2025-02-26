f86873835e44b5824f7ada769acee15b97973e90
baddeep <pradeepargal22@gmail.com>
2025-02-26 22:30:15 +0530

    idk

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,21 +1,12 @@
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-const About = ()=>{
-    return (
-        <h1>aboutpage</h1>
-=======
-=======
->>>>>>> 26a0cf07959427b231db18e5671a889f3f947793
+
 import IntroBox from "../components/about_us/intro";
 import Quotes from "../components/about_us/quotes";
 import PrimaryButton from "../components/about_us/primary_button";
 import SecIntroBox from "../components/about_us/secondary_intro";
 import Number from "../components/about_us/number";
 import ContactSection from "../components/about_us/get_in_touch";
-<<<<<<< HEAD
 
-=======
->>>>>>> 26a0cf07959427b231db18e5671a889f3f947793
+
 
 const About = () => {
     return(<>
@@ -25,13 +16,8 @@ const About = () => {
         <SecIntroBox/>
         <Number/>
         <ContactSection/>
-<<<<<<< HEAD
+</> 
         
-    </>
->>>>>>> Stashed changes
-=======
-    </>
->>>>>>> 26a0cf07959427b231db18e5671a889f3f947793
     )
 }
 export default About;
