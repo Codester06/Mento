b3406f84b57a6114ff160e337f04f2e3cc4a4a88
baddeep <pradeepargal22@gmail.com>
2025-02-27 21:05:54 +0530

    gradient

--- src/components/quotes/quotesText.js
+++ src/components/quotes/quotesText.js
@@ -3,7 +3,14 @@ const QuotesText = (props) =>{
 
     return(
         <>
-        <blockquote class="blockquote"><p>How your life feels is more  important than how it looks.</p></blockquote>
+        <blockquote class="blockquote">
+            <div  className=" displatQ">
+
+            <div className="startQ"></div>
+                <p>How your life feels is more  important than how it looks.</p>
+                <div className="closeQ"></div>
+                </div>
+            </blockquote>
 </>
     )
 
