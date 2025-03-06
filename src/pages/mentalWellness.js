2a4fa4ddf10f994895154c3bc4a55e5cef1b4331
AlphaBlueX <pranavsp1977@gmail.com>
2025-03-06 22:26:59 +0530

    test1

--- src/pages/mentalWellness.js
+++ src/pages/mentalWellness.js
@@ -1,7 +1,28 @@
+import React from 'react';
+import MentalWellnessComponent from '../components/mentalwellness/mentalwellness';
+import '../components/mentalwellness/mentalwellness.css';
+
 const MentalWellness = () => {
-    return(
-        <h1>Mental Wellness </h1>
-    )
+    return (
+        <MentalWellnessComponent>
+            <h1 className="form-title">FORM</h1>
+            <form>
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" id="name" name="name" />
+                </div>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" name="email" />
+                </div>
+                <div>
+                    <label htmlFor="message">Message:</label>
+                    <textarea id="message" name="message"></textarea>
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+        </MentalWellnessComponent>
+    );
 }
 
 export default MentalWellness;
