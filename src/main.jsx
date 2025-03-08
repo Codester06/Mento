db7b726a0ceef9a32f6ca64782afcd3793c13cae
Vivek Sharma <viveksharma.vs0615@gmail.com>
2025-03-08 21:11:44 +0530

    otherPages + mentalWellness

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,8 @@ import About from "./pages/about";
 import ContactForm from "./pages/contact";
 import Faq from "./pages/faq";
 import ExpertDetail from "./pages/ExpertDetail";
+import TermsUsage from "./pages/terms-of-usage";
+import CookiesPolicy from "./pages/cookies-policy";
 
 function Main() {
   return (
@@ -23,9 +25,12 @@ function Main() {
             <Route path="/mental-wellness" element={<MentalWellness />} />
             <Route path="/blogs" element={<Blogs />} />
             <Route path="/about" element={< About/>} />
-            <Route path="/faq" element={<Faq/>} />
+            <Route path="/faqs" element={<Faq/>} />
             <Route path="/expert/:id" element={<ExpertDetail />} />
             <Route path="*" element={<NotFound />} /> {/* 404 Route */}
+            <Route path="/terms-conditions" element={<TermsUsage/>}/>
+            <Route path="/cookies-privacy-policy" element={<CookiesPolicy/>}/>
+
           </Routes>
         </div>
         <Footer />
