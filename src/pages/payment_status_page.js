1d6fd4c383cef57540250e288c2c9d60c6f5b5a5
baddeep <pradeepargal22@gmail.com>
2025-04-17 19:26:11 +0530

    pradeep

---
+++ src/pages/payment_status_page.js
@@ -0,0 +1,11 @@
+import PaymentStatus from "../components/payment/payment_status";
+
+const paymentStatusPage = () => {   
+    return (
+        <div className="payment-status-page">
+        <PaymentStatus />
+        </div>
+    );
+    }   
+
+export default paymentStatusPage;
